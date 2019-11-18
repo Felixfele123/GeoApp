cb7c3cb78bbf82b44a08060ee076ca2b28b025f7
HK <felix.eriksson00@hotmail.com>
2019-11-18 20:14:05 +0100

    -

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <GMap />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import GMap from '@/components/home/GMap'
+import Navbar from '@/components/layout/Navbar'
+
+export default {
+  name: 'App',
+  components: {GMap, Navbar}
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.config.productionTip = false
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTdW60tqLUX4fDraj3hVpJ7lr58TJnxRk",
+  authDomain: "geomap-4db2f.firebaseapp.com",
+  databaseURL: "https://geomap-4db2f.firebaseio.com",
+  projectId: "geomap-4db2f",
+  storageBucket: "geomap-4db2f.appspot.com",
+  messagingSenderId: "376493967107",
+  appId: "1:376493967107:web:97adff077575f15a190b36",
+  measurementId: "G-E00R2DKQMG"
+});
+
+export const db = firebase.firestore();
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
